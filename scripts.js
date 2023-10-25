34e2f25aa4cb19c81216a8bb99eaffdf5b9c02a8
Panonim <58903942+Panonim@users.noreply.github.com>
2023-10-25 14:31:47 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,25 @@ window.addEventListener("load", function() {
   loader.style.display = "none";
 });
 
+var typed1 = new Typed(".aftertext", {
+  strings: ["Blue", "Artur"],
+  typeSpeed: 150,
+  backSpeed: 100,
+  backDelay: 2000,
+  startDelay: 100,
+  loop: true,
+  cursorChar: '_',
+});
 
+var typed2 = new Typed(".learning", {
+  strings: ["javascript", "css", "html"],
+  typeSpeed: 100,
+  backDelay: 3000,
+  startDelay: 50,
+  loop: true,
+  cursorChar: '', 
+  fadeOut: true,
+});
 
 function updateProgressBar() {
   const progressBar = document.querySelector('.progress-bar');
